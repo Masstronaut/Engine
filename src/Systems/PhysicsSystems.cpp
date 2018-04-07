8abcdec89ee618e1953b7fc5dc3e9e8f8e9d9c30
Luc Kadletz <luckadletz@gmail.com>
2018-04-07 02:04:20 -0700

    Basic integration implementation
    aka make stuff move

--- src/Systems/PhysicsSystems.cpp
+++ src/Systems/PhysicsSystems.cpp
@@ -1,25 +1,64 @@
 #include "PhysicsSystems.h"
 
-class PhysicsIntegration
+const float EPSILON = std::numeric_limits<float>::epsilon();
+
+#pragma region Integration
+
+void PhysicsIntegration::Update(float dt)
 {
-	void Update(float dt)
-	{
+	// TODO handle any engine/physics dt magic
+	Dt = dt;
+}
 
-	}
-};
-
-class PhysicsCollisionDetection
+void PhysicsIntegration::Process(Transform & transform, PhysicsBody & body)
 {
-	void Update(float dt)
-	{
+	UpdateVelocities(body);
+	Integrate(transform, body);
+	ApplyGravity(body);
+}
 
-	}
-};
-
-class PhysicsResolution
+void PhysicsIntegration::UpdateVelocities(PhysicsBody & body)
 {
-	void Update(float dt)
-	{
+	body.velocity += body.massInverse * body.impulse;
+	body.impulse = { 0.f, 0.f, 0.f };
 
+	body.angularVelocity += body.inertiaTensor * body.angularImpulse;
+	body.angularImpulse = { 0.f, 0.f, 0.f };
+}
+
+void PhysicsIntegration::ApplyGravity(PhysicsBody & body)
+{
+	const glm::vec3 GRAVITY = { 0.f, -5.0f, 0.f }; // m/s^2, approx half earth's
+
+	if (body.massInverse > EPSILON)
+	{
+		body.impulse += GRAVITY / body.massInverse;
 	}
-};
+}
+
+void PhysicsIntegration::Integrate(Transform & transform, const PhysicsBody & body)
+{
+	transform.position += body.velocity * Dt;
+
+	// TODO 3D Rotation
+}
+
+#pragma endregion
+
+#pragma region Collision
+
+void PhysicsCollisionDetection::Update(float dt)
+{
+
+}
+
+#pragma endregion
+
+#pragma region Resolution
+
+void PhysicsResolution::Update(float dt)
+{
+
+}
+
+#pragma endregion
--- src/Systems/PhysicsSystems.h
+++ src/Systems/PhysicsSystems.h
@@ -10,13 +10,22 @@ class PhysicsIntegration
 public:
 	EntitiesWith<Transform, PhysicsBody> Entities;
 	void Update(float dt);
+	void Process(Transform &transform, PhysicsBody& body);
+	
+private:
+	float Dt;
+
+	void UpdateVelocities(PhysicsBody& body);
+	void ApplyGravity(PhysicsBody& body);
+	void Integrate(Transform &transform, const PhysicsBody& body);
 };
 
 class PhysicsCollisionDetection
 {
 public:
 	EntitiesWith<Transform, PhysicsBody> Entities;
-		// TODO Should be < const Transform, (optional) const PhysicsBody, const IPhysicsCollider >
+	// TODO Should be < const Transform, (optional) const PhysicsBody, const IPhysicsCollider >
+	// TODO EntitiesWith<Manifold> CollidingEntities;
 	void Update(float dt);
 };
 
@@ -24,7 +33,6 @@ class PhysicsResolution
 {
 public:
 	EntitiesWith<Transform, PhysicsBody> Entities;
-	// TODO EntitiesWith<Manifold> CollidingEntities;
 
 	void Update(float dt);
 };
