8cc6c9661fe97cd912da08a5e3b7658299e6cb3e
Luc Kadletz <luckadletz@gmail.com>
2018-04-07 01:31:43 -0700

    Fixed header ref, added Entity fields

--- src/Systems/PhysicsSystems.h
+++ src/Systems/PhysicsSystems.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include "../Entity/EntitiesWith.hpp"
+
+#include "../Components/Transform.h"
+#include "../Components/PhysicsBody.h"
+
+class PhysicsIntegration
+{
+public:
+	EntitiesWith<Transform, PhysicsBody> Entities;
+	void Update(float dt);
+};
+
+class PhysicsCollisionDetection
+{
+public:
+	EntitiesWith<Transform, PhysicsBody> Entities;
+		// TODO Should be < const Transform, (optional) const PhysicsBody, const IPhysicsCollider >
+	void Update(float dt);
+};
+
+class PhysicsResolution
+{
+public:
+	EntitiesWith<Transform, PhysicsBody> Entities;
+	// TODO EntitiesWith<Manifold> CollidingEntities;
+
+	void Update(float dt);
+};
