84465b9c6632cf116119f51e00705a9896a2c97d
Luc Kadletz <luckadletz@gmail.com>
2018-04-08 22:53:17 -0700

    Look at that platforming gameplay ooh wow neat
    :video_game: :video_game: :video_game:

---
+++ Prototypes/SoLongAndTanks/Assets/GravityCenter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class GravityCenter : MonoBehaviour 
+{
+
+	public float magnitude = 10.0f;
+	public AnimationCurve falloff;
+
+	private CircleCollider2D collider;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<CircleCollider2D>();
+		collider.isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Vector3 grav = (other.transform.position - transform.position);
+		float dist = grav.magnitude / collider.radius; // should be [0..1]
+
+		grav = grav.normalized * falloff.Evaluate(dist) * magnitude;
+
+		other.GetComponent<Rigidbody2D>().AddForce(grav, ForceMode2D.Force);
+	}
+
+}
---
+++ Prototypes/SoLongAndTanks/Assets/TankPlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+class TankPlayerController : MonoBehaviour 
+{
+
+	private CircleCollider2D Collider;
+
+	public float TreadSpeed;
+	public float AngCorrectionSpeed;
+	public bool IsGrounded {get; private set;}
+	private Vector2 GroundNormal;
+
+	// Use this for initialization
+	void Start () {
+		Collider = GetComponent<CircleCollider2D>();
+	}
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		IsGrounded = true;
+
+		// Ground normal is average of all contact normals I guess
+		GroundNormal *= 0.0f;
+		foreach(var contact in col.contacts)
+		{
+			GroundNormal += contact.normal;
+		}
+		GroundNormal.Normalize();
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.DrawRay(transform.position, GroundNormal, Color.red, 0.1f);
+	
+		if(!IsGrounded)
+		{
+			return;
+		}
+
+		float targetAngDelt = Vector3.Angle(transform.up, GroundNormal);
+		float angDelt = Mathf.Max(targetAngDelt, AngCorrectionSpeed * Time.deltaTime);
+
+		transform.Rotate(Vector3.forward, angDelt);
+
+		if(Input.GetAxis("Horizontal") > 0.1f)
+		{
+			Vector3 moveForce = TreadSpeed * transform.right;
+			GetComponent<Rigidbody2D>().AddForce(moveForce, ForceMode2D.Force);
+		}
+		else if(Input.GetAxis("Horizontal") < -0.1f)
+		{
+			Vector3 moveForce = -TreadSpeed * transform.right;
+			GetComponent<Rigidbody2D>().AddForce(moveForce, ForceMode2D.Force);
+		}
+	
+	}
+
+
+}
