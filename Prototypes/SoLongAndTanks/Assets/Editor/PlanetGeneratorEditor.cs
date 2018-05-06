ed0fa0c854e02699800386553f01d69d3b20c112
Luc Kadletz <luckadletz@gmail.com>
2018-05-06 00:13:35 -0700

    Added simple planet gen

---
+++ Prototypes/SoLongAndTanks/Assets/Editor/PlanetGeneratorEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlanetGenerator))]
+public class PlanetGeneratorEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		if(GUILayout.Button("TEST"))
+		{
+			// TODO Generate planet at edit time for faster startup and manual editing
+		}
+	}
+}
---
+++ Prototypes/SoLongAndTanks/Assets/PlanetGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGenerator : MonoBehaviour 
+{
+
+	public GameObject TurfPrefab;
+	public float Radius;
+
+	public float Density;
+
+	public float AngNoise;
+	public void Generate()
+	{
+		if(Density <= 0.0f)
+		{
+			Debug.LogError("Density must be positive!");
+			return;
+		}
+		for(float x = -Radius;
+			x <= Radius;
+			x += Density)
+			{
+				for(float y = -Radius;
+					y <= Radius;
+					y += Density)
+				{
+					if(x * x + y * y >= Radius * Radius) continue;
+					float angle = Mathf.Rad2Deg * Mathf.Atan2(y, x);
+					Vector2 pos = new Vector2(transform.position.x + x, transform.position.y + y);
+					CreateTurf(pos, angle);
+				}
+			}
+	}
+	private GameObject CreateTurf(Vector2 pos, float angle)
+	{
+		Quaternion rot = Quaternion.Euler(0, 0, angle + Random.Range(0.0f, AngNoise));
+		GameObject turf = GameObject.Instantiate(TurfPrefab, pos, rot, transform);
+		
+		Rigidbody2D turfBody = turf.GetComponent<Rigidbody2D>();
+		if(turfBody)
+		{
+			turfBody.bodyType = RigidbodyType2D.Static;
+		}
+
+		return turf;
+	}
+
+	public void CleanUp()
+	{
+		// Destroy all child objects
+		for(int i = 0; i < transform.childCount;)
+		{
+			Transform child = transform.GetChild(i);
+			GameObject.DestroyImmediate(child.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		CleanUp();
+		Generate();
+	}
+
+	public void OnDestroy()
+	{
+		CleanUp();
+	}
+
+}
